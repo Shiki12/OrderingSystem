f63580a4760354fd3037735f79ebff70233b7899
sNightwind <yyuu0218@gmail.com>
2022-06-16 19:59:56 +0800

    1

--- src/main/java/com/web/dao/CustomerDao.java
+++ src/main/java/com/web/dao/CustomerDao.java
@@ -1,9 +1,19 @@
 package com.web.dao;
 
+import com.web.entity.Customer;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
-public class CustomerDao {
+public interface CustomerDao {
 
+    int addCustomer(Customer customer);
 
+    List<Customer> getAll();
+
+    Customer getById(@Param("id")  int id);
+
+    int deleteById(@Param("id")  int id);
 }
--- src/main/java/com/web/service/CustomerService.java
+++ src/main/java/com/web/service/CustomerService.java
@@ -8,7 +8,7 @@ public interface CustomerService {
     int addCustomer(Customer customer) ;
     int update(Customer customer);
     List<Customer> getAll();
-    Customer getById( int id) ;
+    Customer getById(int id) ;
     int deleteById(int id);
 
 }
