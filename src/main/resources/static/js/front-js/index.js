0631e7c7b93814a8bf5124874f09a2ea607da9af
ht <3418972095@qq.com>
2022-06-16 18:00:48 +0800

    上传网站，搜索

---
+++ src/main/resources/static/js/front-js/index.js
@@ -0,0 +1,17 @@
+function getdine(){
+    var name1=document.getElementById('dinenum1123').value;
+
+    $.ajax({
+        type: "get",
+        url: 'http://localhost:8001/index/index1',
+        data :{"dinename": name1},
+        dataType: 'json',
+        async: true,
+        success: function (data) {
+            alert(name1)
+        },
+        error() {
+            alert("请求失败！！！")
+        },
+    });
+}
