c416a5c8847737aefae1af7e364987d2fb8b27a7
shiki <941728409@qq.com>
2022-06-26 17:56:23 +0800

    能够响应种类栏的数据

--- src/main/java/com/web/controller/CategoryController.java
+++ src/main/java/com/web/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.web.controller;
 
 import com.web.dao.CategoryDao;
 import com.web.entity.Category;
+import com.web.wechat.dataUtil.ResponseData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,23 +12,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
+@RequestMapping("/category")
 public class CategoryController {
 
     @Autowired
     CategoryDao categoryDao;
 
-@RequestMapping("/category")
-@ResponseBody
-    public String getAll(Model model){
+    @RequestMapping("/getList")
+    @ResponseBody
+    public ResponseData getAll(Model model){
 
         List<Category> categories = categoryDao.queryList();
 
-    for (Category category : categories) {
-        System.out.println(category);
-    }
-        model.addAttribute("list",categories);
+         if (categories.isEmpty()){
+             return new ResponseData(0,"响应失败");
+         }
+         else {
+             return  new ResponseData(1,"响应成功",categories);
+         }
 
-        return "admin/index";
     }
 
 }
